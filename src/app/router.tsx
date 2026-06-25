4e2abd5082f3c2c478d04462486289e3c0fdd9e7
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-06-25 20:07:22 -0300

    feat: implement routing with createBrowserRouter and define routes

---
+++ src/app/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { ProductList } from '../features/products/components/ProductList'
+import HomeLayout from '../features/home/layouts/HomeLayout'
+import HomePage from '../pages/HomePage'
+import RecordProductPage from '../pages/RecordProductPage'
+import { ROUTES } from '../shared/utils/Routes'
+
+export const router = createBrowserRouter([
+
+    {
+        path: "/",
+        Component: HomeLayout,
+
+        children: [
+            {index: true, Component: HomePage},
+            {path: ROUTES.RECORD_PRODUCT, Component: RecordProductPage}
+        ]
+
+    },
+])
