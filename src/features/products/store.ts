8ad7047c9ca927ee567d6dc4200c26efb475d871
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-06-25 20:13:10 -0300

    feat: implement product store with Zustand for state management

---
+++ src/features/products/store.ts
@@ -0,0 +1,76 @@
+﻿import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import type { productsInStateT, productT } from './types'
+
+type productStoreT = {
+  products: productT[];
+  addProduct: (product: productT) => void;
+  updateProduct: (id: string, data: Partial<productT>) => void;
+  deleteProduct: (id: string) => void;
+};
+
+
+export const useProductStore = create<productStoreT> ()(
+
+    persist(
+
+        (set)  => {
+
+            return {
+
+                products: [] ,
+            
+                addProduct: ( data ) => {
+
+                    set( (state : productStoreT ) : productsInStateT => {
+
+                        return {
+
+                            products: [
+                                ...state.products,
+                                {
+                                    ...data,
+                                    id: crypto.randomUUID()
+                                }
+                            ]
+
+                        }
+                    } )
+
+                }
+                ,
+                updateProduct: ( id, data) => {
+
+                    set( (state) : productsInStateT => {
+
+                        return {
+                            products: state.products.map( 
+                                (product) => {
+                                return product.id === id 
+                                                    ? {...product, ...data}
+                                                    : product
+                            } )
+                        }
+                    } )
+
+                }
+                ,
+                deleteProduct: (id) => {
+
+                    set( (state) : productsInStateT => {
+                        return {
+                            products: state.products.filter( (product) => product.id !== id )
+                        }
+                    } )
+
+                }
+
+            }
+
+        },
+        {name: "warranty-storage"}
+
+    )
+
+)
+
