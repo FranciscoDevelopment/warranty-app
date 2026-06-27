75b28a9e440d03be115062583a8edd19d5a4d6ee
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-06-26 21:41:01 -0300

    feat: add Toaster component for improved user notifications

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import './index.css'
+import { Toaster } from "react-hot-toast"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster position='top-right' />
     <RouterProvider router={router} />
   </StrictMode>,
 )
