c9994e423ca06887880480531c02cde16a573a6e
irinadandrea <irinaszareyk@gmail.com>
2026-06-30 21:50:55 -0300

    fix: clean up router configuration by removing unnecessary imports and formatting

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { ProductList } from '../features/products/components/ProductList'
 import HomeLayout from '../features/home/layouts/HomeLayout'
 import HomePage from '../pages/HomePage'
 import RecordProductPage from '../pages/RecordProductPage'
@@ -7,16 +6,13 @@ import RemindersPage from '../pages/RemindersPage'
 import { ROUTES } from '../shared/utils/Routes'
 
 export const router = createBrowserRouter([
-
     {
         path: "/",
         Component: HomeLayout,
-
         children: [
             {index: true, Component: HomePage},
-            {path: ROUTES.RECORD_PRODUCT, Component: RecordProductPage,
-                path: ROUTES.REMINDERS, Component: RemindersPage}, 
+            {path: ROUTES.RECORD_PRODUCT, Component: RecordProductPage},
+            {path: ROUTES.REMINDERS, Component: RemindersPage},
         ]
-
     },
 ])
